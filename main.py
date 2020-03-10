e8ee959849bce4d670aa2d6a8de9c4af85da41e1
Fabian Werner <fabian99.werner@gmail.com>
2020-03-10 01:20:23 +0100

    Include current time in file

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
+import locale
 import re
+import time
+
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium import webdriver
 
 options = Options()
 options.add_argument('--headless')
+# Ohne Ansicht des Browsers starten
 options.binary_location = r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe'
 driver_path: str = r'D:\Programs\BrowserDriver\chromedriver.exe'
 driver: WebDriver = webdriver.Chrome(options=options, executable_path=driver_path)
 driver.get("https://www.alexa.com/topsites/countries/DE")
-print('working')
-# element = driver.find_element_by_class_name('listings table').text
-elem = driver.find_element_by_xpath("//*[@id='alx-content']/div/section/div[1]/section[2]/span/span/div/div").text
-# source_code = elem.get_attribute("tableContainer")
-alexa_rank = []
-alexa_rank.append(elem)
+# Seite für die Top50 Webseiten in Deutschland
+elem: object = driver.find_element_by_xpath(
+    "//*[@id='alx-content']/div/section/div[1]/section[2]/span/span/div/div").text
+# Elemente holen
+alexa_rank = [elem]
 str1 = ''.join(alexa_rank)
-pattern = re.findall('[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}',str1)
-print(pattern)
+pattern = re.findall('[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}', str1)
+# URL extrahieren; string da List kein regex unterstützt
 driver.quit()
-str2 = '\n'.join(pattern)
-print(str2)
+# Browser stoppen
+locale.setlocale(locale.LC_TIME, 'de_DE.UTF-8')
+timestamp = time.strftime("%d %B %Y %H:%M:%S %n", time.localtime())
+# Aktuelle Uhrzeit im deutschen Format
+str2: str = '\n'.join(pattern)
+# String, da list nicht ausgeschrieben werden kann
 with open('D:/Download/Seminararbeit/html_source_code.txt', 'w') as f:
+    # Uhrzeit und Liste in Txt speichern
+    f.write(timestamp)
     f.write(str2)
